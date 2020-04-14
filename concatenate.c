de99bc6021ce8498c811e267ef1f5b905b7c982d
saabircs <63609537+saabircs@users.noreply.github.com>
2020-04-14 10:36:51 +0530

    Implement a program to concatenate two strings without using built in functions

---
+++ concatenate.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i,j;
+    char str1[100],str2[100];
+    printf("Enter first string:");
+    gets(str1);
+    printf("Enter second string:");
+    gets(str2);
+    for (i = 0; str1[i] != '\0'; ++i);
+    for (j = 0; str2[j] != '\0'; ++j, ++i)
+        str1[i] = str2[j];
+    str1[i] = '\0';
+
+    printf("Concatenated string is:");
+    puts(str1);
+    return 0;
+}
